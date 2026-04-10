cf3b195b23efc7653c76c597714765388f6f4cfc
pulkitrox <pulkitg3107@gmail.com>
2026-04-10 16:23:10 +0530

    Fix Flask binding for Render deployment

--- backend/app.py
+++ backend/app.py
@@ -158,4 +158,5 @@ def convert(): # parses the input and extracts all information in required form,
     return redirect(url_for('visualizer'))
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host= "0.0.0.0", port=port)
